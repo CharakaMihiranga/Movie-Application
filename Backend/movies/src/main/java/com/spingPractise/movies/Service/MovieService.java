2d76ce0cb6832aceadad1e37bcb8bce050ffae5d
CharakaMihiranga <charakavmihiranga@gmail.com>
2024-07-02 23:16:09 +0530

    carousel implement:has to add styles to the carousel

--- Backend/movies/src/main/java/com/spingPractise/movies/Service/MovieService.java
+++ Backend/movies/src/main/java/com/spingPractise/movies/Service/MovieService.java
@@ -1,12 +1,16 @@
 package com.spingPractise.movies.Service;
 
 
+import com.mongodb.MongoSocketReadTimeoutException;
 import com.spingPractise.movies.entity.Movie;
 import com.spingPractise.movies.repository.MovieRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
+import java.net.SocketTimeoutException;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,6 +20,8 @@ public class MovieService {
     @Autowired
     private MovieRepository movieRepository;
 
+    @Retryable(value = { MongoSocketReadTimeoutException.class, SocketTimeoutException.class },
+            maxAttempts = 3, backoff = @Backoff(delay = 1000))
     public List<Movie> AllMovies() {
         return movieRepository.findAll();
     }
--- Frontend/MovieClient/movie-gold-v1/src/api/axiosConfig.js
+++ Frontend/MovieClient/movie-gold-v1/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: 'https://7fef-203-189-184-216.ngrok-free.app',
+  baseURL:'https://569b-203-189-184-74.ngrok-free.app',
   headers:{"ngrok-skip-browser-warning": "true"}
 });
